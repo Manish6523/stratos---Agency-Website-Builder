627da143b12900ca848fb5fc382a1ac9f52c750c
Manish6523 <220101005092@gandhinagaruni.ac.in>
2026-02-27 00:45:49 +0530

    feat(funnel): bootstrap editor page with DB fetch and EditorProvider
    - Fetch FunnelPage from DB by funnelPageId
    - Redirect to funnel dashboard if page not found
    - Wrap editor canvas in full-screen EditorProvider container
    - Wire in FunnelEditorNavigation component

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import EditorProvider from "@/providers/editor/editor-provider";
+import FunnelEditorNavigation from "./_components/funnel-editor-navigation";
+// import FunnelEditorNavigation from './_components/FunnelEditorNavigation'
+// import FunnelEditorSidebar from './_components/FunnelEditorSidebar'
+// import FunnelEditor from './_components/FunnelEditor'
 
-type Props = {}
+type Props = {
+  params: Promise<{
+    subaccountId: string;
+    funnelId: string;
+    funnelPageId: string;
+  }>;
+};
 
-const page = (props: Props) => {
+export default async function Page({ params }: Props) {
+  const param = await params;
+
+  const funnelPageDetails = await db.funnelPage.findFirst({
+    where: {
+      id: param.funnelPageId,
+    },
+  });
+
+  if (!funnelPageDetails) {
+    return redirect(
+      `/subaccount/${param.subaccountId}/funnels/${param.funnelId}`,
+    );
+  }
+
   return (
-    <div>page</div>
-  )
-}
+    <div className="fixed top-0 bottom-0 left-0 right-0 z-20 bg-background overflow-hidden">
+      <EditorProvider
+        subaccountId={param.subaccountId}
+        funnelId={param.funnelId}
+        pageDetails={funnelPageDetails}
+      >
+        <FunnelEditorNavigation
+          funnelId={param.funnelId}
+          funnelPageDetails={funnelPageDetails}
+          subaccountId={param.subaccountId}
+        />
+        {/* <div className="h-full flex justify-center">
+          <FunnelEditor funnelPageId={param.funnelPageId} />
+        </div>
 
-export default page
+        <FunnelEditorSidebar subaccountId={param.subaccountId} /> */}
+      </EditorProvider>
+    </div>
+  );
+}
