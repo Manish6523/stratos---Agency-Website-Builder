00dc2938b3eb4df8c0dde8a104610060d7b232d6
Manish6523 <220101005092@gandhinagaruni.ac.in>
2026-03-31 16:06:55 +0530

    feat(subaccount): add automations page route

---
+++ src/app/(main)/subaccount/[subaccountId]/automations/page.tsx
@@ -0,0 +1,14 @@
+import BlurPage from '@/components/global/blur-page'
+import React from 'react'
+
+type Props = {}
+
+const AutomationPage = (props: Props) => {
+  return (
+    <BlurPage>
+        <div>AutomationPage</div>
+    </BlurPage>
+  )
+}
+
+export default AutomationPage
